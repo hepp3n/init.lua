55accd960715705e042898773965a380a6a9f27c
Piotr <114903054+hepp3n@users.noreply.github.com>
2025-01-19 15:07:16 +0100

    update

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ vim.g.python3_host_prog = '/usr/bin/python3'
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 
-vim.cmd 'colorscheme lunaperche'
+-- vim.cmd 'colorscheme lunaperche'
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
--- lua/custom/plugins/rust.lua
+++ lua/custom/plugins/rust.lua
@@ -42,7 +42,6 @@ return {
 
   {
     'mrcjkb/rustaceanvim',
-    version = vim.fn.has 'nvim-0.10.0' == 0 and '^4' or false,
     ft = { 'rust' },
     opts = {
       server = {
@@ -96,18 +95,16 @@ return {
       },
     },
     config = function(_, opts)
-      if vim.has 'mason.nvim' then
-        local package_path = require('mason-registry').get_package('codelldb'):get_install_path()
-        local codelldb = package_path .. '/extension/adapter/codelldb'
-        local library_path = package_path .. '/extension/lldb/lib/liblldb.dylib'
-        local uname = io.popen('uname'):read '*l'
-        if uname == 'Linux' then
-          library_path = package_path .. '/extension/lldb/lib/liblldb.so'
-        end
-        opts.dap = {
-          adapter = require('rustaceanvim.config').get_codelldb_adapter(codelldb, library_path),
-        }
+      local package_path = require('mason-registry').get_package('codelldb'):get_install_path()
+      local codelldb = package_path .. '/extension/adapter/codelldb'
+      local library_path = package_path .. '/extension/lldb/lib/liblldb.dylib'
+      local uname = io.popen('uname'):read '*l'
+      if uname == 'Linux' then
+        library_path = package_path .. '/extension/lldb/lib/liblldb.so'
       end
+      opts.dap = {
+        adapter = require('rustaceanvim.config').get_codelldb_adapter(codelldb, library_path),
+      }
       vim.g.rustaceanvim = vim.tbl_deep_extend('keep', vim.g.rustaceanvim or {}, opts or {})
       if vim.fn.executable 'rust-analyzer' == 0 then
         vim.error('**rust-analyzer** not found in PATH, please install it.\nhttps://rust-analyzer.github.io/', { title = 'rustaceanvim' })
