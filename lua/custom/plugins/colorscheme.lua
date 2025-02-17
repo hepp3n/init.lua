e11f7615886afd69e1168aedb612852d62d5f335
Piotr <114903054+hepp3n@users.noreply.github.com>
2025-02-17 14:57:19 +0100

    update

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,35 +1,46 @@
 return {
   {
-    'rebelot/kanagawa.nvim',
+    'miikanissi/modus-themes.nvim',
     lazy = false,
     priority = 1000,
     config = function()
-      require('kanagawa').setup {
-        compile = false, -- enable compiling the colorscheme
-        undercurl = true, -- enable undercurls
-        commentStyle = { italic = true },
-        functionStyle = {},
-        keywordStyle = { italic = true },
-        statementStyle = { bold = true },
-        typeStyle = {},
-        transparent = false, -- do not set background color
-        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-        terminalColors = true, -- define vim.g.terminal_color_{0,17}
-        colors = { -- add/modify theme and palette colors
-          palette = {},
-          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+      vim.o.background = 'light'
+
+      -- Default options
+      require('modus-themes').setup {
+        -- Theme comes in two styles `modus_operandi` and `modus_vivendi`
+        -- `auto` will automatically set style based on background set with vim.o.background
+        style = 'auto',
+        variant = 'default', -- Theme comes in four variants `default`, `tinted`, `deuteranopia`, and `tritanopia`
+        transparent = false, -- Transparent background (as supported by the terminal)
+        dim_inactive = false, -- "non-current" windows are dimmed
+        hide_inactive_statusline = false, -- Hide statuslines on inactive windows. Works with the standard **StatusLine**, **LuaLine** and **mini.statusline**
+        line_nr_column_background = true, -- Distinct background colors in line number column. `false` will disable background color and fallback to Normal background
+        sign_column_background = true, -- Distinct background colors in sign column. `false` will disable background color and fallback to Normal background
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
         },
-        overrides = function(colors) -- add/modify highlights
-          return {}
-        end,
-        theme = 'dragon', -- Load "wave" theme when 'background' option is not set
-        background = { -- map the value of 'background' option to a theme
-          dark = 'dragon', -- try "dragon" !
-          light = 'lotus',
-        },
+
+        --- You can override specific color groups to use other groups or a hex color
+        --- Function will be called with a ColorScheme table
+        --- Refer to `extras/lua/modus_operandi.lua` or `extras/lua/modus_vivendi.lua` for the ColorScheme table
+        ---@param colors ColorScheme
+        on_colors = function(colors) end,
+
+        --- You can override specific highlights to use other groups or a hex color
+        --- Function will be called with a Highlights and ColorScheme table
+        --- Refer to `extras/lua/modus_operandi.lua` or `extras/lua/modus_vivendi.lua` for the Highlights and ColorScheme table
+        ---@param highlights Highlights
+        ---@param colors ColorScheme
+        on_highlights = function(highlights, colors) end,
       }
 
-      vim.cmd.colorscheme 'kanagawa'
+      vim.cmd.colorscheme 'modus_operandi'
     end,
   },
 }
