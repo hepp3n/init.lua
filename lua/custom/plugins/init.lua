d1c87622689a4a40e77a006ed2534c27fdfc9dce
Piotr <114903054+hepp3n@users.noreply.github.com>
2025-05-05 19:53:13 +0200

    init

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,12 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'mbbill/undotree',
+    config = function()
+      vim.keymap.set('n', '<leader><F5>', vim.cmd.UndotreeToggle, { desc = 'UndoTree' })
+    end,
+  },
+}
