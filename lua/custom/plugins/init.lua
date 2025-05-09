ba0ec5188cacda40795bbc9e7a49d12b725e1017
Piotr <114903054+hepp3n@users.noreply.github.com>
2025-05-09 18:37:24 +0200

    add plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -13,4 +13,30 @@ return {
   {
     'tpope/vim-fugitive',
   },
+
+  {
+    'saecki/crates.nvim',
+    config = function()
+      require('crates').setup {}
+    end,
+  },
+
+  {
+    'linux-cultist/venv-selector.nvim',
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'mfussenegger/nvim-dap',
+      'mfussenegger/nvim-dap-python', --optional
+      { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
+    },
+    lazy = false,
+    branch = 'regexp', -- This is the regexp branch, use this for the new version
+    keys = {
+      { ',v', '<cmd>VenvSelect<cr>' },
+    },
+    ---@type venv-selector.Config
+    opts = {
+      -- Your settings go here
+    },
+  },
 }
