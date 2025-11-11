9e3055284c21fd176c269a7ef1c74ae2932c465e
Piotr <hepp3n@gmail.com>
2025-11-11 17:11:36 +0100

    add update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,6 +4,13 @@ return {
 		"miikanissi/modus-themes.nvim",
 		priority = 1000,
 		config = function()
+			require("modus-themes").setup({
+				-- Theme comes in two styles `modus_operandi` and `modus_vivendi`
+				-- `auto` will automatically set style based on background set with vim.o.background
+				style = "auto",
+				variant = "default", -- Theme comes in four variants `default`, `tinted`, `deuteranopia`, and `tritanopia`
+				transparent = true,
+			})
 			vim.cmd([[colorscheme modus]])
 		end,
 	},
