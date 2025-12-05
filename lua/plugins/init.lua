f88e0a378cf41843896dd7646dfced35dcbf662a
Piotr <hepp3n@gmail.com>
2025-12-05 07:35:20 +0100

    update theme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,17 +1,39 @@
 return {
 	-- Colorscheme
+	-- {
+	-- 	"miikanissi/modus-themes.nvim",
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("modus-themes").setup({
+	-- 			-- Theme comes in two styles `modus_operandi` and `modus_vivendi`
+	-- 			-- `auto` will automatically set style based on background set with vim.o.background
+	-- 			style = "auto",
+	-- 			variant = "default", -- Theme comes in four variants `default`, `tinted`, `deuteranopia`, and `tritanopia`
+	-- 			transparent = true,
+	-- 		})
+	-- 		vim.cmd([[colorscheme modus]])
+	-- 	end,
+	-- },
+
 	{
-		"miikanissi/modus-themes.nvim",
+		"Shatur/neovim-ayu",
 		priority = 1000,
 		config = function()
-			require("modus-themes").setup({
-				-- Theme comes in two styles `modus_operandi` and `modus_vivendi`
-				-- `auto` will automatically set style based on background set with vim.o.background
-				style = "auto",
-				variant = "default", -- Theme comes in four variants `default`, `tinted`, `deuteranopia`, and `tritanopia`
-				transparent = true,
-			})
-			vim.cmd([[colorscheme modus]])
+			-- require("ayu").setup({
+			-- 	overrides = {
+			-- 		Normal = { bg = "None" },
+			-- 		NormalFloat = { bg = "none" },
+			-- 		ColorColumn = { bg = "None" },
+			-- 		SignColumn = { bg = "None" },
+			-- 		Folded = { bg = "None" },
+			-- 		FoldColumn = { bg = "None" },
+			-- 		CursorLine = { bg = "None" },
+			-- 		CursorColumn = { bg = "None" },
+			-- 		VertSplit = { bg = "None" },
+			-- 	},
+			-- })
+
+			vim.cmd([[colorscheme ayu]])
 		end,
 	},
 
